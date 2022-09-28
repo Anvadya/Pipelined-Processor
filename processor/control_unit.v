22662acddd5d74c144a4ff42a124ce5c608a1e51
Aryan5-IITR <93373163+Aryan5-IITR@users.noreply.github.com>
2022-09-28 19:06:42 +0530

    Update control_unit.v

--- processor/control_unit.v
+++ processor/control_unit.v
@@ -8,14 +8,14 @@ module control_unit(
   always @(*) begin
     control_rod[8:0]	<= 9'b000000000;	//Set all the bits of the control_rod to 0
     if(!opcode[3])
-      control_rod[2:0] <= opcode[2:0];		//Conrol_signals for ALU operations
+      assign control_rod[2:0] = opcode[2:0];		//Conrol_signals for ALU operations
     else if(opcode[0]&opcode[1]) begin		//Control Signals for BEQ and JMP
       if(opcode[2]) 
-        control_rod[8]<=1'b1;
+        assign control_rod[8] = 1'b1;
       else
-        control_rod[4]<=1'b1;
+        assign control_rod[4] = 1'b1;
     end else								//Control Signals for LD, ST and RES
-      control_rod[7:5] <= opcode[2:0];
+      assign control_rod[7:5] = opcode[2:0];
   end
 end module 									//control_unit
   
