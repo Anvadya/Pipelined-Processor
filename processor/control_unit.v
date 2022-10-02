eb5e07364419296dde0fa369b9d09d6fca9a1750
Aryan5-IITR <93373163+Aryan5-IITR@users.noreply.github.com>
2022-10-02 15:12:28 +0530

    Update control_unit.v

--- processor/control_unit.v
+++ processor/control_unit.v
@@ -1,21 +1,25 @@
-module control_unit(
-  input wire [3:0]   opcode   			  // First four bits of the instruction
-  input              clk,                 // Clock
+module pipeline(
+  input  [3:0]    opcode,    			  // First four bits of the instruction
+  input            clk,             // Clock
   
-  output wire [8:0]  control_rod          // Set of all control signals 
+  output [8:0]  control_rod         // Set of all control signals 
 );
   
-  always @(*) begin
-    control_rod[8:0]	<= 9'b000000000;	//Set all the bits of the control_rod to 0
+  reg [8:0] cu_reg=9'b000000000;    // Register to store the values of the control signals
+  
+  always @ posedge(clk) begin
+    cu_reg[8:0]	<= 9'b000000000;	  //Set all the bits of the control unit register to 0
     if(!opcode[3])
-      assign control_rod[2:0] = opcode[2:0];		//Conrol_signals for ALU operations
+      cu_reg[2:0] <= opcode[2:0];	  //Conrol_signals for ALU operations and NOP
     else if(opcode[0]&opcode[1]) begin		//Control Signals for BEQ and JMP
       if(opcode[2]) 
-        assign control_rod[8] = 1'b1;
+        cu_reg[8]<=1'b1;
       else
-        assign control_rod[4] = 1'b1;
+        cu_reg[4]<=1'b1;
     end else								//Control Signals for LD, ST and RES
-      assign control_rod[7:5] = opcode[2:0];
+      cu_reg[7:5] <= opcode[2:0];
   end
-end module 									//control_unit
   
+  assign control_rod = cu_reg;      //Attaching the output wire to the control unit register
+
+endmodule
