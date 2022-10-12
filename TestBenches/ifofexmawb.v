2a728502f78f7eee26d1204c375e706097cadb98
Subhajit009iitr <biswassubhajit009@gmail.com>
2022-10-13 03:21:26 +0530

    Better Integration

--- TestBenches/ifofexmawb.v
+++ TestBenches/ifofexmawb.v
@@ -16,27 +16,7 @@ reg clk=0;
 
 wire[8:0] Branch_Update_with_isBranch;
 
-reg[7:0] Branch_Update;
-reg[0:0] is_Branch;
-
-assign Branch_Update_with_isBranch[7:0]=Branch_Update[7:0];
-assign Branch_Update_with_isBranch[8:8]=is_Branch[0:0];
-
-reg[7:0] Address;
-reg[63:0] Value;
-
-reg[0:0] isWrite;
-reg[0:0] isLoad;
-reg[0:0] isMemWrite;
-
 reg[3:0] reg_address;
-
-assign Address_Value_RegAddress_isLoad_isMemWrite_isWrite[7:0]=Address[7:0];
-assign Address_Value_RegAddress_isLoad_isMemWrite_isWrite[71:8]=Value[63:0];
-assign Address_Value_RegAddress_isLoad_isMemWrite_isWrite[75:72]=reg_address[3:0];
-assign Address_Value_RegAddress_isLoad_isMemWrite_isWrite[76:76]=isLoad[0:0];
-assign Address_Value_RegAddress_isLoad_isMemWrite_isWrite[77:77]=isMemWrite[0:0];
-assign Address_Value_RegAddress_isLoad_isMemWrite_isWrite[78:78]=isWrite[0:0];
 
 initial begin
     IfOf=24'd0;
@@ -69,6 +49,10 @@ reg[156:0] OfEx; // OF/EX Register
 
 always @(*) begin
 OfEx<=OF_output;
+end
+
+always @(posedge clk) begin
+$display("OF Output : ",OF_output);
 end
 
 alu al(
@@ -105,33 +89,45 @@ initial begin
     $dumpfile("ifofexmawb.vcd");
     $dumpvars(0,ifofexmawb);
     clk=0;
-    // Branch_Update=8'b00000111;
-    // Address=8'b00010001;
-    // Value=64'd5;
-    // reg_address=4'b0110;
-    // isLoad=1'b0;
-    // isMemWrite=1'b0;
-    // isWrite=1'b1;
-    is_Branch=1'b0;
     #10;
     clk=1;
     #10;
-    // Address=8'b00000000;
-    // Value=64'd0;
-    // reg_address=4'b000;
-    // isLoad=1'b0;
-    // isMemWrite=1'b0;
-    // isWrite=1'b0;
     clk=0;
     #10;
     clk=1;
     #10;
-    // Address=8'b00000010;
-    // Value=64'd3;
-    // reg_address=4'b010;
-    // isLoad=1'b0;
-    // isMemWrite=1'b1;
-    // isWrite=1'b0;
+    clk=0;
+    #10;
+    clk=1;
+    #10;
+    clk=0;
+    #10;
+    clk=1;
+    #10;
+    clk=0;
+    #10;
+    clk=1;
+    #10;
+    clk=0;
+    #10;
+    clk=1;
+    #10;
+    clk=0;
+    #10;
+    clk=1;
+    #10;
+    clk=0;
+    #10;
+    clk=1;
+    #10;
+    clk=0;
+    #10;
+    clk=1;
+    #10;
+    clk=0;
+    #10;
+    clk=1;
+    #10;
     clk=0;
     #10;
     clk=1;
