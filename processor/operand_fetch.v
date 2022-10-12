7e5e1ab0dad86b903c6b4bbe105d9ff7842954b8
jainl28patel <jainilpatel2003@gmail.com>
2022-10-12 17:01:06 +0530

    Added support for 2 new control signals needed for stalling

--- processor/operand_fetch.v
+++ processor/operand_fetch.v
@@ -2,29 +2,20 @@
 `include "register_file.v"
 
 module operand_fetch(
-    // input wire [3:0] pc,            // Program Counter
-    // input wire [15:0] instruction,  // Instruction from instruction memory
-    // input wire [3:0] write_port_address,
     input wire [23:0] IF_output,       
-    // input wire [63:0] write_data,
     input wire clk,
-
-    // output wire [7:0] control_rod,
-    // output wire [7:0] PC,
-    // output wire [63:0] register_read1,
-    // output wire [63:0] register_read2,
-    // output wire        flag,
-    // output wire [7:0] address,
-    // output wire [3:0] reg_to_be_written
+    output wire [1:0] stalling_control_signal,
     output wire [156:0] OF_output      // control_rod (8 bit), PC(8 bit), reg1_data(64 bit), reg2_data(64 bit), flag(1 bit), address(8 bit), reg_write_address(4 bit)
 );
 
-wire [7:0] Control_rod;
+//  OUTPUT DATA
+wire [9:0] Control_rod;
 wire [63:0] reg1_data;
 wire [63:0] reg2_data;
 wire [0:0] Flag;
 reg [3:0] register_to_be_written;
 
+//   GIVING REDUNDENT DATA AS INPUT IN CASE OF REGISTER WRITE
 reg [63:0] write_data;
 reg [3:0] write_port_address;
 reg is_write;
@@ -40,10 +31,14 @@ wire is_write_wire;
 assign write_data_wire = write_data;
 assign write_port_address_wire = write_port_address;
 assign is_write_wire = is_write;
+//  END OF ASSIGNING REDUNDANT DATA 
 
+// Generating Control Signals
 control_unit cu (IF_output[11:8], Control_rod);
+// Invoking register file
 register_file reg_file (IF_output[15:12], IF_output[19:16], write_port_address_wire, write_data_wire, is_write_wire, clk, reg1_data, reg2_data, Flag);
 
+// MUX for the selection of write-back address
 always @(posedge clk) begin
     register_to_be_written = IF_output[15:12];
     if (Control_rod[0]|Control_rod[1]|Control_rod[2]) begin
@@ -51,20 +46,14 @@ always @(posedge clk) begin
     end
 end
 
-// assign control_rod = Control_rod;
-// assign PC = pc;
-// assign register_read1 = reg1_data;
-// assign register_read2 = reg2_data;
-// assign Flag = flag;
-// assign address = instruction[23:16];
-// assign reg_to_be_written = register_to_be_written;
-
-assign OF_output[7:0] = Control_rod;
-assign OF_output[15:8] = IF_output[7:0];
-assign OF_output[79:16] = reg1_data;
-assign OF_output[143:80] = reg2_data;
-assign OF_output[144] = Flag;
-assign OF_output[152:145] = IF_output[23:16];
-assign OF_output[156:153] = register_to_be_written;
+// ASSIGNING OUTPUTS
+assign OF_output[7:0] = Control_rod[7:0];                   // Control Rod
+assign OF_output[15:8] = IF_output[7:0];                    // Program Counter
+assign OF_output[79:16] = reg1_data;                        // Register-1 read data
+assign OF_output[143:80] = reg2_data;                       // Register-2 read data
+assign OF_output[144] = Flag;                               // Flag
+assign OF_output[152:145] = IF_output[23:16];               // Address in case of LW/SW instruction
+assign OF_output[156:153] = register_to_be_written;         // Address of register to be written in write-back
+assign stalling_control_signal[1:0] = Control_rod[9:8];     // Control signal needed for Stalling
 
 endmodule
